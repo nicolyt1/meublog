d7fb7668076046180db07531de3ae448aeee6173
teste <a@a.com>
2018-04-05 17:03:46 -0400

    inserindo dados dinamicos

--- meublog/posts/views.py
+++ meublog/posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from posts.models import Post
+def index(request):
+	posts = Post.objects.all().order_by('created_date')
+	return render(request,'blog.html', {'posts' : posts})
 # Create your views here.
